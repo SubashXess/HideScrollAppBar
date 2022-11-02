6daf24c39d7529e14beb75182bf8a6aafa3f7c7b
Subash Xess <subuxess@gmail.com>
2022-11-03 01:41:39 +0530

    init-changes-2

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -41,7 +40,7 @@ class _HomePageState extends State<HomePage>
       body: NotificationListener<UserScrollNotification>(
         onNotification: (notification) {
           setState(() {
-            if (_scrollController.position.pixels > 20.0) {
+            if (_scrollController.position.pixels > 10.0) {
               _isAppBarShow = false;
               print('false');
             } else if (_scrollController.position.pixels < 20.0) {
@@ -62,29 +61,33 @@ class _HomePageState extends State<HomePage>
                 child: SafeArea(
                   child: Column(
                     children: [
-                      AppBar(
-                        centerTitle: true,
-                        automaticallyImplyLeading: false,
-                        backgroundColor: Colors.white,
-                        elevation: 0.0,
-                        title: const Text(
-                          'Scroll AppBar',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        actions: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.search_rounded,
-                              size: 20.0,
-                              color: Colors.black54,
+                      AnimatedOpacity(
+                        opacity: _isAppBarShow ? 1.0 : 0.0,
+                        duration: const Duration(milliseconds: 50),
+                        child: AppBar(
+                          centerTitle: true,
+                          automaticallyImplyLeading: false,
+                          backgroundColor: Colors.white,
+                          elevation: 0.0,
+                          title: const Text(
+                            'Scroll AppBar',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ],
+                          actions: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.search_rounded,
+                                size: 20.0,
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                       Container(
                         height: 250.0,
@@ -150,39 +153,62 @@ class _HomePageState extends State<HomePage>
                 left: 0,
                 right: 0,
                 child: AnimatedSlide(
-                  duration: const Duration(milliseconds: 250),
+                  duration: const Duration(milliseconds: 100),
                   curve: Curves.easeInOut,
-                  offset: _isAppBarShow ? const Offset(0, -1.0) : Offset.zero,
+                  // opacity: _isAppBarShow ? 0.0 : 1.0,
+                  offset: _isAppBarShow ? const Offset(0.0, -1.0) : Offset.zero,
                   child: AppBar(
-                    centerTitle: true,
                     scrolledUnderElevation: 1.0,
                     leadingWidth: 0.0,
                     titleSpacing: 0.0,
+                    automaticallyImplyLeading: false,
+                    elevation: 0.0,
                     title: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const Text('Scroll AppBar'),
-                        const SizedBox(height: 10.0),
-                        Container(
-                          height: 40.0,
+                        SizedBox(
                           width: size.width,
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Positioned(
+                                left: 0.0,
+                                child: IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.arrow_back,
+                                    size: 20.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              const Text('Scroll AppBar'),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
+                        Container(
+                          width: size.width,
+                          height: 40.0,
                           margin: const EdgeInsets.symmetric(horizontal: 16.0),
                           child: TextFormField(
                             decoration: InputDecoration(
-                              isDense: true,
+                              isDense: false,
                               hintText: 'Search',
                               filled: true,
                               fillColor: Colors.white54,
+                              contentPadding: EdgeInsets.zero,
+                              prefixIcon: const Icon(
+                                Icons.search_rounded,
+                                size: 20.0,
+                                color: Colors.black45,
+                              ),
                               hintStyle: const TextStyle(
                                   color: Colors.black45, fontSize: 14.0),
                               border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  borderSide: BorderSide.none),
-                              prefixIcon: const Icon(
-                                Icons.search_rounded,
-                                size: 18.0,
-                                color: Colors.black45,
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide.none,
                               ),
                             ),
                           ),
@@ -201,100 +227,3 @@ class _HomePageState extends State<HomePage>
     );
   }
 }
-
-// AnimatedSlide(
-          //         duration: duration,
-          //         curve: Curves.easeInOut,
-          //         offset: _isAppBarShow ? const Offset(0, -1.0) : Offset.zero,
-          //         child: Container(
-          //           width: double.infinity,
-          //           height: kToolbarHeight * 4,
-          //           decoration: const BoxDecoration(color: Colors.deepPurple),
-          //           child: const SafeArea(
-          //             child: Text('Scroll AppBar'),
-          //           ),
-          //         ),
-          //       ),
-
-
-//  CustomScrollView(
-//         slivers: [
-//           const SliverAppBar(
-//             title: Text(
-//               'Scroll AppBar',
-//               style: TextStyle(color: Colors.black),
-//             ),
-//             automaticallyImplyLeading: false,
-//             expandedHeight: kTextTabBarHeight,
-//             backgroundColor: Colors.white,
-//             scrolledUnderElevation: 1.0,
-//             floating: false,
-//             pinned: true,
-//           ),
-//           SliverToBoxAdapter(
-//             child: Container(
-//               height: 250.0,
-//               margin: const EdgeInsets.all(16.0),
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(16.0),
-//                 color: Colors.deepPurple.shade200,
-//               ),
-//               child: const Center(
-//                 child: Text('data'),
-//               ),
-//             ),
-//           ),
-//           SliverToBoxAdapter(
-//             child: Container(
-//               height: 250.0,
-//               margin: const EdgeInsets.all(16.0),
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(16.0),
-//                 color: Colors.deepPurple.shade200,
-//               ),
-//               child: const Center(
-//                 child: Text('data'),
-//               ),
-//             ),
-//           ),
-//           SliverToBoxAdapter(
-//             child: Container(
-//               height: 250.0,
-//               margin: const EdgeInsets.all(16.0),
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(16.0),
-//                 color: Colors.deepPurple.shade200,
-//               ),
-//               child: const Center(
-//                 child: Text('data'),
-//               ),
-//             ),
-//           ),
-//           SliverToBoxAdapter(
-//             child: Container(
-//               height: 250.0,
-//               margin: const EdgeInsets.all(16.0),
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(16.0),
-//                 color: Colors.deepPurple.shade200,
-//               ),
-//               child: const Center(
-//                 child: Text('data'),
-//               ),
-//             ),
-//           ),
-//           SliverToBoxAdapter(
-//             child: Container(
-//               height: 250.0,
-//               margin: const EdgeInsets.all(16.0),
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(16.0),
-//                 color: Colors.deepPurple.shade200,
-//               ),
-//               child: const Center(
-//                 child: Text('data'),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
